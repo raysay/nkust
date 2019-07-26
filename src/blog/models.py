e34e2a46c39b5c08f54d96ce50655af42895b626
Rex <rexshiu@gmail.com>
2019-07-26 10:01:29 +0800

    img of userprofile

--- src/blog/models.py
+++ src/blog/models.py
@@ -6,6 +6,6 @@ class post(models.Model):
     title = models.CharField(max_length=255)
     rec_time = models.DateTimeField()
     content = models.TextField()
-    poster = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
+    poster = models.ForeignKey(UserProfile, related_name='posts', on_delete = models.CASCADE)
     def __str__(self):
         return self.title
