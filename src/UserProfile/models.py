6767b8380b117b1edb4fe317bc226856a59ee02c
Rex <rexshiu@gmail.com>
2019-07-26 09:25:09 +0800

    img

--- src/UserProfile/models.py
+++ src/UserProfile/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class UserProfile(models.Model):
     name = models.CharField(max_length=40)
-
+    img = models.ImageField(upload_to="profile", null=True, blank=True)
     def __str__(self):
         return self.name
--- src/nkust/urls.py
+++ src/nkust/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include,re_path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -31,3 +33,5 @@ urlpatterns = [
     # path('accounts/', include('django.contrib.auth.urls')),
     re_path(r'^accounts/', include('allauth.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
