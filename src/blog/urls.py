33cbd0ac980c100477f4ba518a3f0f343527167e
Rex <rexshiu@gmail.com>
2019-07-26 10:50:49 +0800

    add detail view

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path,include,re_path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     # path('home/', views.as_view(), name='home'),
     path('post/', views.post, name='post'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -3,3 +3,7 @@ from . import models
 def post(request):
     posts = models.post.objects.all()
     return render(request,'blog/post.html',{'posts':posts})
+
+def post_detail(request,pk):
+    post = models.post.objects.get(pk=pk)
+    return render(request,'blog/post_detail.html',{'post':post})
