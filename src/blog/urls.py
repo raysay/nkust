ec36b4dcca5bef2e292b80b69df5673c8967aa44
jim96388 <you@example.com>
2019-07-26 11:44:16 +0800

    test3

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path,include,re_path
 from . import views
 
+appname='post'
 urlpatterns = [
+
     # path('home/', views.as_view(), name='home'),
     path('post/', views.post, name='post'),
+    path('post/<int:blog>/', views.post_detail, name='post_detail'),
 ]
